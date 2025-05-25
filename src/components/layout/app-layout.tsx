394f3901003d8eafbeffd7db21b9c425a4df0968
Roberto Pagni <robypag@me.com>
2025-05-26 01:15:52 +0200

    Update app-layout.tsx

--- src/components/layout/app-layout.tsx
+++ src/components/layout/app-layout.tsx
@@ -4,7 +4,6 @@ import { SidebarProviders, useRightSidebar } from "@/hooks/use-sidebar-states";
 import { AppSidebar } from "./app-sidebar";
 import { RightSidebar } from "./right-sidebar";
 import { Navbar } from "./navbar";
-import { cn } from "@/lib/utils";
 
 interface AppLayoutProps {
     children: React.ReactNode;
@@ -21,15 +20,15 @@ function AppLayoutContent({ children }: AppLayoutProps) {
                 <main className="flex-1 overflow-auto bg-gray-50 dark:bg-gray-900">
                     <div className="container mx-auto p-6 max-w-7xl">{children}</div>
                 </main>
-                
+
                 {/* Backdrop overlay */}
                 {isRightSidebarOpen && (
-                    <div 
+                    <div
                         className="absolute inset-0 bg-black/20 z-40 transition-opacity duration-200"
                         onClick={toggleRightSidebar}
                     />
                 )}
-                
+
                 {/* Right sidebar */}
                 <div className="absolute top-0 right-0 h-full z-50">
                     <RightSidebar />
